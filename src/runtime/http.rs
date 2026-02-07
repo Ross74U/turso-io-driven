9a5b4c52729ab37f9a36c86eb596504d373e7f36
Ross74U <jasonshen0518@gmail.com>
2026-02-06 21:29:45 -0800

    implemented basic http/1.1 server

--- src/runtime/http.rs
+++ src/runtime/http.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
+use libc::{close, CLONE_SETTLS};
+use std::future::Pending;
 use std::sync::Arc;
 use super::{Runtime, Program, ProgramWaker, unwrap_completion};
 use crate::io::completion::{Completion, SharedCompletion, AppCompletion};
 use crate::io::generic::{ServerSocket, ClientConnection};
 use tracing::{info};
+use httparse::{Request, Status};
+use http::{Response, StatusCode};
 
 pub struct HttpServer {
     server_sock: Arc<dyn ServerSocket>,
@@ -52,55 +56,143 @@ impl Program for HttpServer {
     }
 }
 
+enum ClientState {
+    Receiving,
+    Responding
+}
+
 pub struct HandleHttpClient {
+    state: ClientState,
     conn: Arc<dyn ClientConnection>,
     parent: Runtime, // parent runtime
+    req_buf: Vec<u8>,
     completion: Option<SharedCompletion>,
+    resp_state_machine: Option<HttpResponseState>
 }
 
 impl HandleHttpClient {
     pub fn new(conn: Arc<dyn ClientConnection>, parent: Runtime) -> Box<Self> {
-        Box::new(HandleHttpClient { conn, parent, completion: None })
+        let req_buf = Vec::with_capacity(8*1024); //8KB
+        Box::new(HandleHttpClient { 
+            state: ClientState::Receiving,
+            conn,
+            parent,
+            completion: None,
+            resp_state_machine: None,
+            req_buf,
+        })
+    }
+
+    fn close_connection(&mut self, waker: ProgramWaker) {
+        info!("closing connection");
+        if let Some(id) = waker.id() {
+            self.parent.deregister(id);
+        }
     }
 }
  
 impl Program for HandleHttpClient {
     fn step(&mut self, waker: ProgramWaker) -> Result<()> {
-        let mut eof = false;
-        let mut text_buf = Vec::new();
-
-        if let Some(c) = self.completion.as_ref() {
-            unwrap_completion!(
-                c == AppCompletion::Recv,
-                |c| { 
-                    if c.result() == Some(0) {
-                        eof = true;
-                    } else {
-                        text_buf = c.buf().to_owned();
-                    }
-                },
-                { unreachable!() }
-            );
-        }
-        
-        if !text_buf.is_empty() {
-            // echo text back with no callback on completion
-            let null_waker = ProgramWaker { program_id: None, run_queue: self.parent.run_queue.clone()};
-            let sendc = Arc::new(Completion::AppCompletion(AppCompletion::new_send(null_waker, text_buf)));
-            self.conn.send(sendc.clone())?;
-        }
-
-        if eof {
-            // TODO: cleanup (close fd, remove self from programs)
-            if let Some(id) = waker.id() {
-                self.parent.deregister(id);
+        match self.state {
+            ClientState::Responding => {
+                let Some(resp_state) = self.resp_state_machine.as_mut() else {unreachable!()};
+                match resp_state.step(waker.clone())? {
+                    State::Pending => {}
+                    State::Complete(_) => {self.close_connection(waker);} // we've sent everything, close
+                    // connection
+                } 
             }
-            return Ok(());
+            ClientState::Receiving => {
+                if let Some(c) = self.completion.as_ref() {
+                    unwrap_completion!(
+                        c == AppCompletion::Recv,
+                        |c| { 
+                            if c.result() == Some(0) {
+                                // eof 
+                            } else {
+                                self.req_buf.extend(c.buf());
+                            }
+                        },
+                        { unreachable!() }
+                    );
+                }
+
+                let mut headers = [httparse::EMPTY_HEADER; 64];
+                let mut req = Request::new(&mut headers);
+                match req.parse(&self.req_buf) {
+                    Ok(Status::Partial) => { 
+                        let recvc = Arc::new(Completion::AppCompletion(AppCompletion::new_recv(waker.clone(), 64)));
+                        self.conn.recv(recvc.clone())?;
+                        self.completion = Some(recvc);
+                    }
+                    Ok(Status::Complete(_body_offset)) => { 
+                        info!("complete! responding");
+                        let mut resp_state = HttpResponseState::new(self.conn.clone());
+                        resp_state.step(waker.clone())?; // step once to initiate (lazy)
+                        self.resp_state_machine = Some(resp_state);
+                        self.state = ClientState::Responding;
+                    }
+                    Err(_error) => { todo!() }
+                } 
+            }
         }
-         
-        let recvc = Arc::new(Completion::AppCompletion(AppCompletion::new_recv(waker, 64)));
-        self.conn.recv(recvc.clone())?;
-        self.completion = Some(recvc);
+
         Ok(())
     }
+}
+
+
+enum State<T> {
+    Complete(T),
+    Pending
+}
+
+/// state machine (sub-future in HandleHttpClient program)
+struct HttpResponseState {
+    conn: Arc<dyn ClientConnection>,
+    completion: Option<SharedCompletion>,
+}
+
+impl HttpResponseState { 
+    fn new(conn: Arc<dyn ClientConnection>) -> Self {
+        Self { conn, completion: None }
+    }
+    fn step(&mut self, waker: ProgramWaker) -> Result<State<()>> {
+        info!("stepping response machine");
+        if self.completion.is_none() {
+            let body = b"hello world";
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header("content-type", "text/plain")
+                .header("content-length", format!("{}", body.len()))
+                .body(())?;
+            let mut buf = Vec::new();
+            encode_http1_response_head(&response, &mut buf);
+            buf.extend_from_slice(body);
+            let sendc = Arc::new(Completion::AppCompletion(AppCompletion::new_send(waker, buf)));
+            self.conn.send(sendc.clone())?;
+            self.completion = Some(sendc);
+            Ok(State::Pending)
+        } else {
+            Ok(State::Complete(()))
+        }
+    }
+}
+
+pub fn encode_http1_response_head<B>(resp: &Response<B>, out: &mut Vec<u8>) {
+    let code = resp.status().as_u16();
+    let reason = resp.status().canonical_reason().unwrap_or("");
+    out.extend_from_slice(b"HTTP/1.1 ");
+    out.extend_from_slice(code.to_string().as_bytes());
+    out.extend_from_slice(b" ");
+    out.extend_from_slice(reason.as_bytes());
+    out.extend_from_slice(b"\r\n");
+
+    for (name, value) in resp.headers().iter() {
+        out.extend_from_slice(name.as_str().as_bytes());
+        out.extend_from_slice(b": ");
+        out.extend_from_slice(value.as_bytes());
+        out.extend_from_slice(b"\r\n");
+    }
+    out.extend_from_slice(b"\r\n");
 }
